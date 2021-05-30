eea60109030922718b3f105463cbcf5270a9893b
Pravpro <praviradlakha@hotmail.com>
2021-05-30 18:25:27 -0400

    Added orders page and basic routes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Load all packages
 const dotenv 				= require('dotenv').config(),
-	  fs 					= require('fs'),
-	  express				= require('express'),
-	  session				= require('express-session'),
-	  crypto 				= require('crypto'),
-	  querystring 			= require('querystring'),
-	  axios 				= require('axios'),
-	  path					= require('path'),
-	  mongoose				= require('mongoose'),
-	  Account 				= require("./models/account");
-	  formidableMiddleware 	= require('express-formidable');
+      fs 					= require('fs'),
+      express				= require('express'),
+      session				= require('express-session'),
+      crypto 				= require('crypto'),
+      querystring 			= require('querystring'),
+      axios 				= require('axios'),
+      path					= require('path'),
+      mongoose				= require('mongoose'),
+      Account 				= require("./models/account");
+      formidableMiddleware 	= require('express-formidable');
 
 const app = express();
 const { encrypt, decrypt } = require('./helpers/crypto');
@@ -19,8 +19,8 @@ const fileUploadDirPath = `${__dirname}/public/${fileUploadFolder}`;
 
 // Connect to DB
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ymgd0.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
-	useNewUrlParser: true, 
-	useUnifiedTopology: true 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
 })
 .then(() => console.log("Connected to DB!"))
 .catch(err => console.log(err));
@@ -29,20 +29,20 @@ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ymgd0.mongodb
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/public`));
 app.use(formidableMiddleware({
-	uploadDir: fileUploadDirPath,
-	keepExtensions: true,
-	multiples: true
+    uploadDir: fileUploadDirPath,
+    keepExtensions: true,
+    multiples: true
 }));
 // Session Management
 app.use(session({
-	name: "sid",
-	secret: "Creating Products really fast is key!",
-	resave: false,
-	saveUninitialized: false,
-	cookie: {
-		samesite: true, // To prevent CSRF
-		secure: NODE_ENV === 'prod'
-	}
+    name: "sid",
+    secret: "Creating Products really fast is key!",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        samesite: true, // To prevent CSRF
+        secure: NODE_ENV === 'prod'
+    }
 }));
 
 // Routes
@@ -50,189 +50,224 @@ const authenticationRoutes 	= require(path.resolve('./', path.join('routes', 'au
 app.use('/shopify', authenticationRoutes);
 
 app.get('/', (req, res) => {
-	console.log(req.session);
-	res.render('index', {shop: req.session.shop});
+    console.log(req.session);
+    res.render('index', {shop: req.session.shop});
 });
 
 // Show login page
 app.get('/login', (req, res) => {
-	res.render('login');
+    res.render('login');
 });
 app.get('/logout', (req, res) => {
-	delete req.session.shop;
-	res.redirect('/')
+    delete req.session.shop;
+    res.redirect('/')
 })
 
 // Get route for batch upload Form
 app.get('/batch/new', isLoggedIn, async (req, res) => {
-	try {
-		// Get locations
-		let data = {
-			query: `
-				query {
-				  locations (first: 20) { 
-				    edges {
-				      node {
-				        id
-				        name
-				      }
-				    } 
-				  } 
-				}
-			`
-		}
-		let response = await makeApiCall(req.session.shop, data);
-		
-		// Render new batch page
-		res.render('batch/new', {shop: req.session.shop, locations: response.data.data.locations});
-	
-	} catch(e) {
-		// TODO: Error Handling
-		console.log(e);
-	}
+    try {
+        // Get locations
+        let data = {
+            query: `
+                query {
+                  locations (first: 20) { 
+                    edges {
+                      node {
+                        id
+                        name
+                      }
+                    } 
+                  } 
+                }
+            `
+        }
+        let response = await makeApiCall(req.session.shop, data);
+        
+        // Render new batch page
+        res.render('batch/new', {shop: req.session.shop, locations: response.data.data.locations});
+    
+    } catch(e) {
+        // TODO: Error Handling
+        console.log(e);
+    }
 });
 
 // Post route for uploading a product
 app.post('/batch/product/new', isLoggedIn, async (req, res) => {
-	try {
-		let baseImageURL = `https://${req.headers.host}/${fileUploadFolder}`;
-		
-		// Rename the files to original names and modify path to reflect this
-		if(!Array.isArray(req.files.images)) req.files.images = [req.files.images];
-		await Promise.all(req.files.images.map( async image => {
-			let newPath = `${fileUploadDirPath}/${image.name}`
-			await fs.rename(image.path, newPath, async () => {
-				image.path = newPath;
-			});
-		}));
-		
-		// Build 'CreateMediaInput' list
-		let imageList = '[';
-		for(let i = 0; i < req.files.images.length; i++) {
-			if(i > 0) { imageList += ','}
-			let encodedImageURL = encodeURI(`${baseImageURL}/${req.files.images[i].name}`);
-			imageList += `{
-				originalSource: "${encodedImageURL}", 
-				mediaContentType: IMAGE
-			}`
-		}
-		imageList += ']';
-		console.log(`[CreateMediaInput]: ${imageList}`);
+    try {
+        let baseImageURL = `https://${req.headers.host}/${fileUploadFolder}`;
+        
+        // Rename the files to original names and modify path to reflect this
+        if(!Array.isArray(req.files.images)) req.files.images = [req.files.images];
+        await Promise.all(req.files.images.map( async image => {
+            let newPath = `${fileUploadDirPath}/${image.name}`
+            await fs.rename(image.path, newPath, async () => {
+                image.path = newPath;
+            });
+        }));
+        
+        // Build 'CreateMediaInput' list
+        let imageList = '[';
+        for(let i = 0; i < req.files.images.length; i++) {
+            if(i > 0) { imageList += ','}
+            let encodedImageURL = encodeURI(`${baseImageURL}/${req.files.images[i].name}`);
+            imageList += `{
+                originalSource: "${encodedImageURL}", 
+                mediaContentType: IMAGE
+            }`
+        }
+        imageList += ']';
+        console.log(`[CreateMediaInput]: ${imageList}`);
 
-		/* Create Product
-		 *	Input: title, description, images
-		 *	Output: id
-		 */
-		let data = {
-			query: `
-				mutation {
-				  productCreate(
-				  	input: { 
-				  	  title: "${req.fields.title}", 
-				  	  descriptionHtml: "${req.fields.description}"
-				  	},
-					media: ${imageList}
-				  )
-				  {
-				    product {
-				      id
-				    }
-				    userErrors {
-				      field
-				      message
-				    }
-				  }
-				}
-			`
-		}
-		let response = await makeApiCall(req.session.shop, data);
-		console.log(JSON.stringify(response.data.data));
-		// let id = response.data.data.productCreate.product.id;
-		
-		// TODO: Do proper error handling
-		if(response.data.data.productCreate.userErrors.length > 0) console.log(response.data.data.productCreate.userErrors.message);
-		
+        /* Create Product
+         *	Input: title, description, images
+         *	Output: id
+         */
+        let data = {
+            query: `
+                mutation {
+                  productCreate(
+                    input: { 
+                      title: "${req.fields.title}", 
+                      descriptionHtml: "${req.fields.description}"
+                    },
+                    media: ${imageList}
+                  )
+                  {
+                    product {
+                      id
+                    }
+                    userErrors {
+                      field
+                      message
+                    }
+                  }
+                }
+            `
+        }
+        let response = await makeApiCall(req.session.shop, data);
+        console.log(JSON.stringify(response.data.data));
+        // let id = response.data.data.productCreate.product.id;
+        
+        // TODO: Do proper error handling
+        if(response.data.data.productCreate.userErrors.length > 0) console.log(response.data.data.productCreate.userErrors.message);
+        
 
-		// Delete Images from server after wait time (= 10 seconds * # of images = total wait time)
-		// TODO: Figure out if there is a better method for when to delete files 
-		setTimeout(() => {
-			for(let i = 0; i < req.files.images.length; i++) {
-				fs.unlink(req.files.images[i].path, err => {
-					if(err){
-						console.log(err);
-						return
-					}
-				});
-			}
-		}, req.files.images.length*10000);
+        // Delete Images from server after wait time (= 10 seconds * # of images = total wait time)
+        // TODO: Figure out if there is a better method for when to delete files 
+        setTimeout(() => {
+            for(let i = 0; i < req.files.images.length; i++) {
+                fs.unlink(req.files.images[i].path, err => {
+                    if(err){
+                        console.log(err);
+                        return
+                    }
+                });
+            }
+        }, req.files.images.length*10000);
 
-		/* Set additional parameters on the product
-		 *	Input: Inventory (prod id, location id, quantity)
-		 */
-		// let id = response.data.data.productCreate.product.id;
-		// TODO: Set Inventory
-		// let quantityRes = await Promise.all( Object.keys(req.fields.quantity).map( async location => {
-		// 	data = {
-		// 		query: `
-		// 			mutation {
-		// 			  inventoryActivate(
-		// 			  	inventoryItemId: "${id}", 
-		// 			  	locationId: "${location}", 
-		// 			  	available: ${req.fields.quantity[location]}
-		// 			  ) 
-		// 			  {
-		// 			  	inventoryLevel{
-		// 			  	  available
-		// 			  	}
-		// 			  	userErrors {
-		// 			      field
-		// 			      message
-		// 			    }
-		// 			  }
-		// 			}
-		// 		`
-		// 	}
-		// 	let qtyRes = await makeApiCall(req.session.shop, data);
-		// 	console.log(qtyRes.data);
-		// 	return qtyRes.data;
-		// }));
-		res.send("Success");
-	} catch(e) {
-		console.log(e);
-	}
+        /* Set additional parameters on the product
+         *	Input: Inventory (prod id, location id, quantity)
+         */
+        // let id = response.data.data.productCreate.product.id;
+        // TODO: Set Inventory
+        // let quantityRes = await Promise.all( Object.keys(req.fields.quantity).map( async location => {
+        // 	data = {
+        // 		query: `
+        // 			mutation {
+        // 			  inventoryActivate(
+        // 			  	inventoryItemId: "${id}", 
+        // 			  	locationId: "${location}", 
+        // 			  	available: ${req.fields.quantity[location]}
+        // 			  ) 
+        // 			  {
+        // 			  	inventoryLevel{
+        // 			  	  available
+        // 			  	}
+        // 			  	userErrors {
+        // 			      field
+        // 			      message
+        // 			    }
+        // 			  }
+        // 			}
+        // 		`
+        // 	}
+        // 	let qtyRes = await makeApiCall(req.session.shop, data);
+        // 	console.log(qtyRes.data);
+        // 	return qtyRes.data;
+        // }));
+        res.send("Success");
+    } catch(e) {
+        console.log(e);
+    }
 });
+
+app.get('/orders', isLoggedIn, (req, res) => {
+    res.render('orders');
+})
+
+app.post('/orders', isLoggedIn, async (req, res) => {
+    console.log(req.fields);
+    data = {
+        query: `
+            query {
+              orders(first: 10, query:"created_at:>=${req.fields.start} created_at:<=${req.fields.end}") {
+                edges {
+                  node {
+                    id
+                    createdAt
+                    shippingAddress {
+                      id
+                      firstName
+                      lastName
+                      address1
+                      address2
+                      city
+                      country
+                      zip
+                    }
+                  }
+                }
+              }
+            }
+        `
+    }
+    let response = await makeApiCall(req.session.shop, data);
+    console.log(response.data.data.orders.edges);
+    res.send("Success");
+})
 
 // TODO: Move these functions out to a helper file.
 // ================================== HELPER FUNCTIONS ==================================
 // TODO: Make this function recognize type of request
 function isLoggedIn(req, res, next) {
-	if(req.session.shop) {
-		return next();
-	}
-	res.redirect('/login');
+    if(req.session.shop) {
+        return next();
+    }
+    res.redirect('/login');
 }
 
 async function makeApiCall(shop, data) {
-	try {
-		// Get AccessToken for shop
-		const foundShop = await Account.find({ shop: shop });
-		if (foundShop.length == 0 ) {throw "ERR: Can't find the shop in Accounts."}
+    try {
+        // Get AccessToken for shop
+        const foundShop = await Account.find({ shop: shop });
+        if (foundShop.length == 0 ) {throw "ERR: Can't find the shop in Accounts."}
 
-		let config = {
-			headers: {
-				'X-Shopify-Access-Token': decrypt(foundShop[0].accessToken)
-			}
-		}
+        let config = {
+            headers: {
+                'X-Shopify-Access-Token': decrypt(foundShop[0].accessToken)
+            }
+        }
 
-		// Make request
-		let response = await axios.post(`https://${shop}/admin/api/2021-04/graphql.json`, data, config)
-		return response;
-	} catch(e) {
-		throw e;
-	}
+        // Make request
+        let response = await axios.post(`https://${shop}/admin/api/2021-04/graphql.json`, data, config)
+        return response;
+    } catch(e) {
+        throw e;
+    }
 }
 
 // START APP
 app.listen(PORT, () => {
-	console.log(`Product Uploader App listening on port ${PORT}!`);
+    console.log(`Product Uploader App listening on port ${PORT}!`);
 });
---
+++ public/scripts/orders.js
@@ -0,0 +1,30 @@
+let startDate, endDate;
+
+let showToast = () => {
+    // Initialize toast
+    var myAlert = $('.toast');//select id of toast
+    var bsAlert = new bootstrap.Toast(myAlert[0]);//inizialize it
+    bsAlert.show();//show it
+}
+let getOrders = async() => {
+    // Post to endpoint that will handle Shopify API call
+    let res = await axios.post('/orders', {'start': startDate, 'end': endDate});
+    console.log(res.data);
+}
+
+// Run functions on document ready
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'left'
+    }, function(start, end, label) {
+        startDate = start;
+        endDate = end;
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+});
+
+// handler for export button click
+$('#export-btn').click(() => {
+    // Check if date range has been set
+    !startDate ? showToast() : getOrders();
+})
