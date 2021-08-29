bfac18722c0b9deb8b4cbab52f940f02b73c7c70
Pravir Adlakha <praviradlakha@hotmail.com>
2021-08-29 01:04:29 -0400

    Modified schema setup to incorporate sku

--- models/account.js
+++ models/account.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-// Schema setup
-let accountSchema = new mongoose.Schema({
+// Format Schema setup
+const FormatSegmentSchema = new Schema({
+	name: String,
+	type: { type: String },
+	data: Map
+});
+
+// Account schema setup
+const AccountSchema = new Schema({
 	shop: String,
-	accessToken: Object
+	accessToken: Object,
+	sku: {
+		format: [FormatSegmentSchema],
+		record: { type: Map, of: Number }
+	}
 });
 
 // Model
-module.exports = mongoose.model("Account", accountSchema);
+module.exports = mongoose.model("Account", AccountSchema);
